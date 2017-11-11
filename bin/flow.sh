894789e57f4459690a3e102e63b1f0f594e8f061
Your Name <you@example.com>
2017-11-11 10:46:27 +0000

    refact and properly waitfor ssh

--- bin/flow.sh
+++ bin/flow.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-set -e
+set -ex
 
 ENV_ID=$1
 ansible-playbook -v -e env_id=${ENV_ID} /opt/playbooks/provision/playbook.yml
-sleep 120
-ansible-playbook -v -e env_id=${ENV_ID} /opt/playbooks/workflow/prepare.yml
+ansible-playbook -vvv -e env_id=${ENV_ID} /opt/playbooks/workflow/prepare.yml
 ansible-playbook -v -e env_id=${ENV_ID} /opt/playbooks/workflow/deployment.yml
 ansible-playbook -v -e env_id=${ENV_ID} /opt/playbooks/workflow/configure.yml
 ansible-playbook -v -e env_id=${ENV_ID} /opt/playbooks/workflow/migrate.yml
